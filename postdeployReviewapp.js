cf31bb7079d067545462e00be04056f6963f39ff
Kube02 <Kube02@sableinternational.com>
2020-08-20 23:34:33 +0200

    heroku-client

--- postdeployReviewapp.js
+++ postdeployReviewapp.js
@@ -4,6 +4,23 @@ console.log('javascript works now');
 console.log('database_url: ' + process.env.DATABASE_URL);
 console.log('HEROKU_API_TOKEN: ' + process.env.HEROKU_API_TOKEN);
 
-var heroku = PlatformAPI.connect_oauth(process.env.HEROKU_API_TOKEN)
-  heroku.config_var.update(process.env.DATABASE_URL, 'xxx')
-  console.log('database_url: ' + process.env.DATABASE_URL);
+
+exec('npm install heroku-client --save',
+    (err, stdout, stderr) => {
+      if (err) return console.error(`Error: ${stderr}`);
+      
+      const Heroku = require('heroku-client')
+      const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN })
+
+      // PATCH requests with body
+        heroku.patch('/apps/' + process.env.HEROKU_APP_NAME + '/config-vars',
+            {
+                body: {
+                    "FOO": "bar",
+                    "BAZ": "qux"
+                }
+            }).then(app => {console.log('FOO: ' + process.env.FOO)  })
+ 
+
+    }
+  );
