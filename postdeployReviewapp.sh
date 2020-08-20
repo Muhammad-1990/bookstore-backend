809dfe0c870a04677032d7d7f90e389572105b0b
Kube02 <Kube02@sableinternational.com>
2020-08-20 14:35:48 +0200

    trying a different method for config vars

--- postdeployReviewapp.sh
+++ postdeployReviewapp.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-
-$ chmod +x postdeployReviewapp.sh
-
-heroku config:set GITHUB_USERNAME=muhammad-1990
+#!/usr/bin/env bash
 
 echo $HEROKU_APP_NAME
-
 export SERVER_URL=https://$HEROKU_APP_NAME.herokuapp.com/parse
