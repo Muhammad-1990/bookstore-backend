521009c48ab66406cebe7718cfd3fd050c880d55
Kube02 <Kube02@sableinternational.com>
2020-08-20 22:23:50 +0200

    node script with heroku config

--- postdeployReviewapp.js
+++ postdeployReviewapp.js
@@ -1,4 +1,12 @@
-
+const { exec } = require("child_process");
 
 console.log('javascript works now');
 console.log('database_url: ' + process.env.DATABASE_URL);
+
+exec('heroku config:set TEST_ENV=hello_config',
+    (err, stdout, stderr) => {
+      if (err) return console.error(`Error: ${stderr}`);
+
+      console.log('it worked: ' + process.env.TEST_ENV);
+    }
+  );
