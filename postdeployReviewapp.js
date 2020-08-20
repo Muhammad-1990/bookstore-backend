39eb012f57c2198cb5fcb26cec5e7472373360f7
Kube02 <Kube02@sableinternational.com>
2020-08-21 00:33:51 +0200

    https request with full config vars

--- postdeployReviewapp.js
+++ postdeployReviewapp.js
@@ -1,62 +1,60 @@
-const { exec } = require("child_process");
-
-console.log('javascript works now');
-console.log('database_url: ' + process.env.DATABASE_URL);
-console.log('HEROKU_API_TOKEN: ' + process.env.HEROKU_API_TOKEN);
-
-
-// exec('npm install heroku-client --save',
-//     (err, stdout, stderr) => {
-//       if (err) return console.error(`Error: ${stderr}`);
-      
-//       const Heroku = require('heroku-client')
-//       const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN })
-
-//       // PATCH requests with body
-//         heroku.patch('/apps/' + process.env.HEROKU_APP_NAME + '/config-vars',
-//             {
-//                 body: {
-//                     "FOO": "bar",
-//                     "BAZ": "qux"
-//                 }
-//             }).then(app => {console.log('FOO: ' + process.env.FOO)  })
- 
-
-//     }
-//   );
-
 const https = require('https');
+const url = require('url');
+
+/**
+ * eg. DATABASE_URL = postgres://ebitxebvixeeqd:dc59b16dedb3a1eef84d4999sb4baf@ec2-50-37-231-192.compute-2.amazonaws.com: 5432/d516fp1u21ph7b
+ * It's read like so: postgres:// USERNAME : PASSWORD @ HOST : PORT : DATABASE_NAME
+ */
+let settings = {
+    client: 'postgres'
+};
+
+const parsed = url.parse(process.env.DATABASE_URL, true);
+const [username, password] = parsed.auth.split(':');
+
+settings.host = parsed.hostname;
+settings.port = Number(parsed.port);
+settings.database = parsed.pathname.substr(1);
+settings.username = username;
+settings.password = password;
+settings.ssl = (parsed.query.ssl === 'true');
 
 var options = {
-  'method': 'PATCH',
-  'hostname': 'api.heroku.com',
-  'path': '/apps/'+process.env.HEROKU_APP_NAME+'/config-vars',
-  'headers': {
-    'Content-Type': 'application/json',
-    'Accept': 'application/vnd.heroku+json; version=3',
-    'Authorization': 'Bearer ' + process.env.HEROKU_API_TOKEN
-  },
-  'maxRedirects': 20
+    'method': 'PATCH',
+    'hostname': 'api.heroku.com',
+    'path': '/apps/' + process.env.HEROKU_APP_NAME + '/config-vars',
+    'headers': {
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.heroku+json; version=3',
+        'Authorization': 'Bearer ' + process.env.HEROKU_API_TOKEN
+    }
 };
 
 var req = https.request(options, function (res) {
-  var chunks = [];
+    var chunks = [];
 
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
+    res.on("data", function (chunk) {
+        chunks.push(chunk);
+    });
 
-  res.on("end", function (chunk) {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
+    res.on("end", function (chunk) {
+        var body = Buffer.concat(chunks);
+        console.log(body.toString());
+    });
 
-  res.on("error", function (error) {
-    console.error(error);
-  });
+    res.on("error", function (error) {
+        console.error(error);
+    });
 });
 
-var postData = JSON.stringify({"abc":"xyz"});
+var postData = JSON.stringify(
+    {
+        "DATABASE_USERNAME": settings.username,
+        "DATABASE_PASSWORD": settings.password,
+        "DATABASE_HOST": settings.host,
+        "DATABASE_PORT": settings.port,
+        "DATABASE_NAME": settings.database
+    });
 
 req.write(postData);
 
