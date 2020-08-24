c6095ee7c88f982e8798275397de826706e2a318
Kube02 <Kube02@sableinternational.com>
2020-08-24 09:40:23 +0200

    added bootstrapped admin

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,4 +10,38 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = async () => {
+
+    // Required fields:
+    const params = {
+      username: process.env.ADMIN_USER || 'admin',
+      password: process.env.ADMIN_PASS || 'admin',
+      email: process.env.ADMIN_EMAIL || 'mo@novationtech.co.za',
+      blocked: false
+    };
+  
+    // Check if exist any admin account - NOTE: you can change this query to find by specific email
+    const admins = await strapi.query('administrator', 'admin').find({ _limit: 1 });
+    if (admins.length) {
+      console.error('You can\'t register a new admin');
+    } else {
+  
+      // Hash password before storing in the database
+      params.password = await strapi.admin.services.auth.hashPassword(params.password);
+  
+      try {
+        // Create admin account
+        const admin = await strapi.query('administrator', 'admin').create({
+            username: params.username,
+            password: params.password,
+            email: params.email,
+            blocked: params.blocked
+        });
+  
+        console.info('(Admin) Account created:', admin);
+  
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
