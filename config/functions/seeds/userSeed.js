119e492440b637952f99652604d73d59ec88aaa8
Kube02 <Kube02@sableinternational.com>
2020-09-04 15:49:11 +0200

    comments

--- config/functions/seeds/userSeed.js
+++ config/functions/seeds/userSeed.js
@@ -1,3 +1,12 @@
+/**
+ * An asynchronous seed function that runs before
+ * your application gets started.
+ *
+ * This gives you an opportunity to set up your data model,
+ * run jobs, or perform some special logic.
+ *
+ * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
+ */
 module.exports.createUser = async () => {
   // Required fields:
   const params = {
